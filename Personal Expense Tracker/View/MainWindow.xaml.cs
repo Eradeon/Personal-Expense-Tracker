55dfe1632bc0661ea05ce5fd23c3949931c31b8a
Eradeon <96534280+Eradeon@users.noreply.github.com>
2022-01-27 11:29:07 +0100

    Added DatePicker style

--- Personal Expense Tracker/View/MainWindow.xaml.cs
+++ Personal Expense Tracker/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
 using System.Windows;
 
 
@@ -14,6 +16,11 @@ namespace Personal_Expense_Tracker.View
             this.DataContext = dataContext;
 
             InitializeComponent();
+
+            //Because trying to format DatePicker string in XAML does not work as required
+            CultureInfo cultureInfo = CultureInfo.CreateSpecificCulture(CultureInfo.CurrentCulture.Name);
+            cultureInfo.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
         }
     }
 }
